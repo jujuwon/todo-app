1b0ef771ddcc73cd1fe2e0eac9676c97ac0f1a45
jujuwon <webmon@kakao.com>
2022-01-19 17:27:31 +0900

    Add User service

---
+++ backend/src/main/java/com/example/todoapp/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.todoapp.service;
+
+import com.example.todoapp.model.UserEntity;
+import com.example.todoapp.persistence.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserEntity create(final UserEntity userEntity) {
+        if(userEntity == null || userEntity.getEmail() == null) {
+            throw new RuntimeException("Invalid arguments");
+        }
+        final String email = userEntity.getEmail();
+        if(userRepository.existsByEmail(email)) {
+            log.warn("Email already exists {}", email);
+            throw new RuntimeException("Email already exists");
+        }
+
+        return userRepository.save(userEntity);
+    }
+}
